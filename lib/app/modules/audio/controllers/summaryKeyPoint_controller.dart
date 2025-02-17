0207d8724f3b592166cd0e2d2763124c49a62a78
Raihan <raihanrafi992@gmail.com>
2025-02-17 14:40:08 +0600

    generate pdf and share done

---
+++ lib/app/modules/audio/controllers/summaryKeyPoint_controller.dart
@@ -0,0 +1,215 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share_plus/share_plus.dart';
+
+import '../../../data/database_helper.dart';
+
+class SummaryKeyPointController extends GetxController {
+  final String keyPoints;
+  final String fileName;
+
+  SummaryKeyPointController({required this.keyPoints, required this.fileName});
+
+  late TextEditingController titleController;
+  late TextEditingController dateController;
+  List<Map<String, String>> mainPoints = [];
+  List<Map<String, String>> conclusions = [];
+  List<TextEditingController> mainPointTitleControllers = [];
+  List<TextEditingController> mainPointValueControllers = [];
+  List<TextEditingController> conclusionTitleControllers = [];
+  List<TextEditingController> conclusionValueControllers = [];
+  RxBool isEditing = false.obs;
+  RxBool isLoading = true.obs; // Add a loading state
+  String parsedDate = "Unknown Date";
+
+  @override
+  void onInit() {
+    super.onInit();
+    // Initialize controllers immediately
+    titleController = TextEditingController();
+    dateController = TextEditingController();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    try {
+      final dbHelper = DatabaseHelper();
+      final db = await dbHelper.database;
+
+      final result = await db.query(
+        'audio_files',
+        columns: ['parsed_date'],
+        where: 'file_name = ?',
+        whereArgs: [fileName],
+      );
+
+      if (result.isNotEmpty) {
+        parsedDate = result.first['parsed_date']?.toString() ?? "Unknown Date";
+      }
+
+      final Map<String, dynamic> data = json.decode(keyPoints);
+      titleController.text = data["Title"]?.toString() ?? "No Title";
+      dateController.text = parsedDate;
+
+      mainPoints = (data["Main Points"] as List?)
+              ?.map((e) => (e as Map<String, dynamic>).map(
+                  (key, value) => MapEntry(key.toString(), value.toString())))
+              .toList() ??
+          [];
+
+      conclusions = (data["Conclusions"] as List?)
+              ?.map((e) => (e as Map<String, dynamic>).map(
+                  (key, value) => MapEntry(key.toString(), value.toString())))
+              .toList() ??
+          [];
+
+      _initializeControllers();
+    } catch (e) {
+      Get.snackbar("Error", "Failed to load data: $e");
+    } finally {
+      isLoading.value = false; // Data loading is complete
+    }
+  }
+
+  void _initializeControllers() {
+    mainPointTitleControllers = mainPoints
+        .map((point) => TextEditingController(text: point.keys.first))
+        .toList();
+    mainPointValueControllers = mainPoints
+        .map((point) => TextEditingController(text: point.values.first))
+        .toList();
+    conclusionTitleControllers = conclusions
+        .map((point) => TextEditingController(text: point.keys.first))
+        .toList();
+    conclusionValueControllers = conclusions
+        .map((point) => TextEditingController(text: point.values.first))
+        .toList();
+  }
+
+  Future<void> saveKeyPoints() async {
+    final dbHelper = DatabaseHelper();
+    final db = await dbHelper.database;
+
+    for (int i = 0; i < mainPoints.length; i++) {
+      mainPoints[i] = {
+        mainPointTitleControllers[i].text: mainPointValueControllers[i].text
+      };
+    }
+
+    for (int i = 0; i < conclusions.length; i++) {
+      conclusions[i] = {
+        conclusionTitleControllers[i].text: conclusionValueControllers[i].text
+      };
+    }
+
+    final updatedData = {
+      "Title": titleController.text,
+      "Main Points": mainPoints,
+      "Conclusions": conclusions,
+    };
+
+    final updatedJson = json.encode(updatedData);
+
+    await db.update(
+      'audio_files',
+      {
+        'key_point': updatedJson,
+        'parsed_date': dateController.text,
+      },
+      where: 'file_name = ?',
+      whereArgs: [fileName],
+    );
+
+    Get.snackbar("Success", "Key Points updated successfully!");
+    isEditing.value = false;
+  }
+
+  Future<void> pickDateTime() async {
+    DateTime? pickedDate = await showDatePicker(
+      context: Get.context!,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+
+    if (pickedDate != null) {
+      TimeOfDay? pickedTime = await showTimePicker(
+        context: Get.context!,
+        initialTime: TimeOfDay.now(),
+      );
+
+      if (pickedTime != null) {
+        DateTime finalDateTime = DateTime(
+          pickedDate.year,
+          pickedDate.month,
+          pickedDate.day,
+          pickedTime.hour,
+          pickedTime.minute,
+        );
+
+        dateController.text =
+            DateFormat('yyyy-MM-dd HH:mm:ss').format(finalDateTime);
+      }
+    }
+  }
+
+  /// Generates and shares the PDF
+  Future<void> generateAndSharePdf() async {
+    final pdf = pw.Document();
+
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) => pw.Column(
+          crossAxisAlignment: pw.CrossAxisAlignment.start,
+          children: [
+            pw.Text(titleController.text,
+                style: pw.TextStyle(
+                  fontSize: 20,
+                ),
+                textAlign: pw.TextAlign.center),
+            pw.SizedBox(height: 20),
+            pw.Text(
+              "Key Points :",
+              style: pw.TextStyle(fontSize: 18),
+            ),
+            pw.SizedBox(height: 10),
+            for (var point in mainPoints)
+              pw.Text("${point.keys.first}:\n${point.values.first}\n\n",
+                  style: pw.TextStyle(fontSize: 14)),
+            pw.SizedBox(height: 10),
+            pw.Text(
+              "Conclusions :",
+              style: pw.TextStyle(fontSize: 18),
+            ),
+            pw.SizedBox(height: 10),
+            for (var conclusion in conclusions)
+              pw.Text("${conclusion.keys.first}:\n${conclusion.values.first}\n\n",
+                  style: pw.TextStyle(fontSize: 14)),
+          ],
+        ),
+      ),
+    );
+
+    final output = await getTemporaryDirectory();
+    final file = File("${output.path}/summary.pdf");
+    await file.writeAsBytes(await pdf.save());
+
+    await Share.shareXFiles([XFile(file.path)], text: "Summary Key Points PDF");
+  }
+
+  @override
+  void onClose() {
+    titleController.dispose();
+    dateController.dispose();
+    mainPointTitleControllers.forEach((controller) => controller.dispose());
+    mainPointValueControllers.forEach((controller) => controller.dispose());
+    conclusionTitleControllers.forEach((controller) => controller.dispose());
+    conclusionValueControllers.forEach((controller) => controller.dispose());
+    super.onClose();
+  }
+}
--- lib/app/modules/audio/views/summary_key_point_view.dart
+++ lib/app/modules/audio/views/summary_key_point_view.dart
@@ -1,245 +1,149 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../../common/customFont.dart';
-import '../../../../common/widgets/auth/custom_button.dart';
 import '../../../../common/widgets/customAppBar.dart';
-import '../../../data/database_helper.dart';
+import '../controllers/summaryKeyPoint_controller.dart';
 
-class SummaryKeyPointView extends StatefulWidget {
+class SummaryKeyPointView extends StatelessWidget {
   final String keyPoints;
-  final String fileName; // Identify which file is being edited
+  final String fileName;
 
   const SummaryKeyPointView({super.key, required this.keyPoints, required this.fileName});
 
   @override
-  _SummaryKeyPointViewState createState() => _SummaryKeyPointViewState();
-}
-
-class _SummaryKeyPointViewState extends State<SummaryKeyPointView> {
-  late TextEditingController titleController;
-  List<Map<String, String>> mainPoints = [];
-  List<Map<String, String>> conclusions = [];
-  List<TextEditingController> mainPointTitleControllers = [];
-  List<TextEditingController> mainPointValueControllers = [];
-  List<TextEditingController> conclusionTitleControllers = [];
-  List<TextEditingController> conclusionValueControllers = [];
-  RxBool isEditing = false.obs; // Toggle Edit Mode
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Parse JSON string
-    final Map<String, dynamic> data = json.decode(widget.keyPoints);
-
-    // Initialize title
-    titleController = TextEditingController(text: data["Title"]?.toString() ?? "No Title");
-
-    // Convert and initialize key points
-    mainPoints = (data["Main Points"] as List?)
-        ?.map((e) => (e as Map<String, dynamic>)
-        .map((key, value) => MapEntry(key.toString(), value.toString())))
-        .toList() ??
-        [];
-
-    // Convert and initialize conclusions
-    conclusions = (data["Conclusions"] as List?)
-        ?.map((e) => (e as Map<String, dynamic>)
-        .map((key, value) => MapEntry(key.toString(), value.toString())))
-        .toList() ??
-        [];
-
-    // Initialize controllers for key points and conclusions
-    _initializeControllers();
-  }
-
-  void _initializeControllers() {
-    mainPointTitleControllers = mainPoints.map((point) {
-      return TextEditingController(text: point.keys.first);
-    }).toList();
-
-    mainPointValueControllers = mainPoints.map((point) {
-      return TextEditingController(text: point.values.first);
-    }).toList();
-
-    conclusionTitleControllers = conclusions.map((point) {
-      return TextEditingController(text: point.keys.first);
-    }).toList();
-
-    conclusionValueControllers = conclusions.map((point) {
-      return TextEditingController(text: point.values.first);
-    }).toList();
-  }
-
-  @override
-  void dispose() {
-    titleController.dispose();
-    for (var controller in mainPointTitleControllers) {
-      controller.dispose();
-    }
-    for (var controller in mainPointValueControllers) {
-      controller.dispose();
-    }
-    for (var controller in conclusionTitleControllers) {
-      controller.dispose();
-    }
-    for (var controller in conclusionValueControllers) {
-      controller.dispose();
-    }
-    super.dispose();
-  }
-
-  // Function to save changes to database
-  Future<void> saveKeyPoints() async {
-    final dbHelper = DatabaseHelper();
-    final db = await dbHelper.database;
-
-    // Update main points with new values
-    for (int i = 0; i < mainPoints.length; i++) {
-      mainPoints[i] = {
-        mainPointTitleControllers[i].text: mainPointValueControllers[i].text
-      };
-    }
-
-    // Update conclusions with new values
-    for (int i = 0; i < conclusions.length; i++) {
-      conclusions[i] = {
-        conclusionTitleControllers[i].text: conclusionValueControllers[i].text
-      };
-    }
-
-    // Construct updated JSON
-    final updatedData = {
-      "Title": titleController.text,
-      "Main Points": mainPoints,
-      "Conclusions": conclusions,
-    };
-
-    final updatedJson = json.encode(updatedData);
-
-    // Update the database
-    await db.update(
-      'audio_files',
-      {'key_point': updatedJson},
-      where: 'file_name = ?',
-      whereArgs: [widget.fileName],
-    );
-
-    // Show success message
-    Get.snackbar("Success", "Key Points updated successfully!");
-
-    // Exit edit mode
-    isEditing.value = false;
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final controller = Get.put(SummaryKeyPointController(keyPoints: keyPoints, fileName: fileName));
+
     return Scaffold(
       appBar: CustomAppBar(
         title: "CLEVERTALK",
         onFirstIconPressed: () {},
         onSecondIconPressed: () {},
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Toggle Edit Button
-              Align(
-                alignment: Alignment.centerRight,
-                child: Obx(() => IconButton(
-                  icon: Icon(
-                    isEditing.value ? Icons.save : Icons.edit,
-                    color: Colors.blue,
-                    size: 28,
-                  ),
-                  onPressed: () {
-                    if (isEditing.value) {
-                      saveKeyPoints(); // Save when exiting edit mode
-                    }
-                    isEditing.toggle(); // Toggle edit mode
-                  },
-                )),
-              ),
+      body: Obx(() {
+        if (controller.isLoading.value) {
+          return Center(child: CircularProgressIndicator()); // Show loading indicator
+        }
 
-              // Editable Title
-              Obx(() => isEditing.value
-                  ? TextField(
-                controller: titleController,
-                style: h1.copyWith(fontSize: 22, fontWeight: FontWeight.bold),
-                decoration: InputDecoration(
-                  labelText: "Title",
-                  border: OutlineInputBorder(),
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    if(!controller.isEditing.value)
+                      IconButton(
+                        icon: Icon(
+                          Icons.share_outlined,
+                          color: Colors.blue,
+                          size: 28,
+                        ),
+                        onPressed: () {
+                          print(':::::::::::::keyPoints::::::::::::::::${controller.titleController.text}');
+                          print(':::::::::::::main points::::::::::::::::${controller.mainPoints}');
+                          print(':::::::::::::conclusions::::::::::::::::${controller.conclusions}');
+                          // pdf generate
+                          controller.generateAndSharePdf();
+                        },
+                      ),
+                    IconButton(
+                      icon: Icon(
+                        controller.isEditing.value ? Icons.save : Icons.edit,
+                        color: Colors.blue,
+                        size: 28,
+                      ),
+                      onPressed: () {
+                        if (controller.isEditing.value) {
+                          controller.saveKeyPoints();
+                        }
+                        controller.isEditing.toggle();
+                      },
+                    ),
+                  ],
                 ),
-              )
-                  : Text(
-                titleController.text,
-                style: h1.copyWith(fontSize: 22, fontWeight: FontWeight.bold),
-              )),
-              SizedBox(height: 20),
-
-              // Key Points
-              Text(
-                'Key Points:',
-                style: h2.copyWith(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              Obx(() => isEditing.value
-                  ? _buildEditableList(mainPoints, mainPointTitleControllers, mainPointValueControllers)
-                  : _buildReadOnlyList(mainPoints)),
-
-              SizedBox(height: 20),
-
-              // Conclusions
-              if (conclusions.isNotEmpty) ...[
+                Obx(() => controller.isEditing.value
+                    ? TextField(
+                  controller: controller.dateController,
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  decoration: InputDecoration(
+                    labelText: "Date",
+                    border: OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.calendar_month_outlined),
+                      onPressed: controller.pickDateTime,
+                    ),
+                  ),
+                )
+                    : Text(
+                  "Date: ${controller.dateController.text}",
+                  style: h4.copyWith(fontSize: 15),
+                )),
+                SizedBox(height: 10),
+                Obx(() => controller.isEditing.value
+                    ? TextField(
+                  controller: controller.titleController,
+                  style: h4.copyWith(fontSize: 18, fontWeight: FontWeight.bold),
+                  decoration: InputDecoration(
+                    labelText: "Title",
+                    border: OutlineInputBorder(),
+                  ),
+                )
+                    : Text(
+                  controller.titleController.text,
+                  style: h4.copyWith(fontSize: 18, fontWeight: FontWeight.bold),
+                )),
+                SizedBox(height: 10),
                 Text(
-                  'Conclusions:',
-                  style: h2.copyWith(fontSize: 20, fontWeight: FontWeight.bold),
+                  'Key Points:',
+                  style: h4.copyWith(fontSize: 17, fontWeight: FontWeight.bold),
                 ),
                 SizedBox(height: 10),
-                Obx(() => isEditing.value
-                    ? _buildEditableList(conclusions, conclusionTitleControllers, conclusionValueControllers)
-                    : _buildReadOnlyList(conclusions)),
+                Obx(() => controller.isEditing.value
+                    ? _buildEditableList(controller.mainPoints, controller.mainPointTitleControllers, controller.mainPointValueControllers)
+                    : _buildReadOnlyList(controller.mainPoints)),
+                SizedBox(height: 20),
+                if (controller.conclusions.isNotEmpty) ...[
+                  Text(
+                    'Conclusions:',
+                    style: h4.copyWith(fontSize: 17, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Obx(() => controller.isEditing.value
+                      ? _buildEditableList(controller.conclusions, controller.conclusionTitleControllers, controller.conclusionValueControllers)
+                      : _buildReadOnlyList(controller.conclusions)),
+                ],
+                SizedBox(height: 30),
               ],
-
-              SizedBox(height: 30),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 
-  // Builds Editable List for Key Points & Conclusions (Title + Value)
   Widget _buildEditableList(List<Map<String, String>> list, List<TextEditingController> titleControllers, List<TextEditingController> valueControllers) {
     return Column(
       children: List.generate(list.length, (index) {
-        TextEditingController titleController = titleControllers[index];
-        TextEditingController valueController = valueControllers[index];
-
         return Padding(
           padding: const EdgeInsets.only(left: 10, bottom: 12),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Title Field
               TextField(
-                controller: titleController,
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                controller: titleControllers[index],
+                style: h4.copyWith(fontSize: 15, fontWeight: FontWeight.bold),
                 decoration: InputDecoration(
                   labelText: "Key Point Title",
                   border: OutlineInputBorder(),
                 ),
               ),
               SizedBox(height: 16),
-
-              // Value Field
               TextField(
-                controller: valueController,
-                style: h4.copyWith(fontSize: 16),
+                controller: valueControllers[index],
+                style: h4.copyWith(fontSize: 15),
                 maxLines: 2,
                 decoration: InputDecoration(
                   labelText: "Description",
@@ -253,22 +157,18 @@ class _SummaryKeyPointViewState extends State<SummaryKeyPointView> {
     );
   }
 
-  // Builds Read-Only List for Viewing Mode
   Widget _buildReadOnlyList(List<Map<String, String>> list) {
     return Column(
       children: list.map((point) {
-        String key = point.keys.first;
-        String value = point.values.first;
-
         return Padding(
           padding: const EdgeInsets.only(left: 10, bottom: 12),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("• $key", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              Text("• ${point.keys.first}", style: h4.copyWith(fontSize: 16, fontWeight: FontWeight.bold)),
               Padding(
                 padding: const EdgeInsets.only(left: 20, top: 4),
-                child: Text(value, style: h4.copyWith(fontSize: 18)),
+                child: Text(point.values.first, style: h4.copyWith(fontSize: 15)),
               ),
             ],
           ),
